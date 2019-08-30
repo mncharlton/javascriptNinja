875851d437879cf106e491ee79408654f1b8c578
Matt Charlton <Matt.charlton@noredink.com>
2019-08-30 11:17:38 +0100

    strings

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,7 +1,35 @@
-alert('hello')
-console.log('sandbox ran!')
+// alert('hello')
+// console.log('sandbox ran!')
+//
+// let age = 31
+// let year = 2019
+//
+// console.log(age, year)
 
-let age = 31
-let year = 2019
+//Strings
+console.log("hello!")
+let name='Matt'
+console.log(name)
 
-console.log(age, year)
+//concatenation
+let firstname = 'Billy'
+let surname = 'Butcher'
+
+let fullname = firstname + ' ' + surname
+console.log(fullname)
+
+//get character
+console.log(fullname[0])
+
+//get length
+console.log(fullname.length)
+
+//methods
+console.log(fullname.toUpperCase())
+let result = fullname.toLowerCase()
+console.log(result)
+
+console.log(fullname)
+
+let index = name.indexOf('M')
+console.log(index)
