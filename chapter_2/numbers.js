f9bcb750a176d9fa85edba2c597e60547dcd0419
Matt Charlton <Matt.charlton@noredink.com>
2019-08-30 11:33:37 +0100

    numbers

--- chapter_2/numbers.js
+++ chapter_2/numbers.js
@@ -2,3 +2,30 @@ let radius = 10
 const pi = 3.14
 
 console.log(radius, pi)
+
+let remainder = radius % 3
+console.log(remainder)
+
+let circleArea = pi * radius ** 2
+console.log(circleArea)
+
+let likes = 10
+likes = likes + 1
+likes++
+likes--
+console.log(likes)
+
+likes += 10
+console.log(likes)
+
+likes *= 2
+console.log(likes)
+
+
+//NaN = not a number
+console.log(5 * 'hello')
+
+
+//concatenation
+let likesCount = 'the blog has ' + likes + ' likes'
+console.log(likesCount)
