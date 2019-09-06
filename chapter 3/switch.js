bfdd5a23e7d25ecaab8bf3c7c1319ad5c7259874
Matt Charlton <Matt.charlton@noredink.com>
2019-09-06 10:04:32 +0100

    end of chapter 3, chapter 4 setup

---
+++ chapter 3/switch.js
@@ -0,0 +1,19 @@
+const grade = 'D'
+
+switch(grade){
+  case 'A':
+    console.log('aced it!')
+    break
+  case 'B':
+    console.log('did ok!')
+    break
+  case 'C':
+    console.log('not bad!')
+    break
+  case 'D':
+    console.log('pretty poor!')
+    break
+  default:
+    console.log('not a valid grade!')
+    break
+}
