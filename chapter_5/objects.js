b47ca3bd419f4822d689bbb9b0e57761eaa98a7c
Matt Charlton <Matt.charlton@noredink.com>
2019-09-09 12:11:08 +0100

    objects

--- chapter_5/objects.js
+++ chapter_5/objects.js
@@ -1,13 +1,29 @@
+const blogs = [
+  {title: 'what is the sky?', likes: 30},
+  {title: 'why is blue?', likes: 20}
+]
+
+console.log(blogs)
+
 let user = {
   name: 'matt',
   age: 31,
   location: 'carlisle',
-  blogs: ['what is the sky?', 'why is blue"?'],
+  blogs: [
+    {title: 'what is the sky?', likes: 30},
+    {title: 'why is blue?', likes: 20}
+  ],
   login: function(){
     console.log('the user logged in')
   },
   logout: function(){
     console.log('the user logged out')
+  },
+  logBlogs(){ //THIS IS A REGULAR FUNCTION
+    //console.log(this.blogs)
+    this.blogs.forEach(blog => {
+      console.log(blog.title, blog.likes)
+    })
   }
 }
 
@@ -22,3 +38,19 @@ user['name'] = 'matthew'
 console.log(user['name'])
 
 user.login()
+user.logBlogs()
+
+console.log(Math)
+console.log(Math.PI)
+console.log(Math.E)
+
+const area = 7.7
+console.log(Math.round(area)) //round normally
+console.log(Math.floor(area)) //take down to integer
+console.log(Math.ceil(area)) //take up to integer
+console.log(Math.trunc(area)) //remove decimal
+
+// random numbers
+const random = Math.random()
+console.log(random) // random between 0 and 1
+console.log(Math.round(random * 100)) // random number between 0 and 1
