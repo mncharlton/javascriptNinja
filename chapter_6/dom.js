5a19b9952ad231205678f6e552c07f2e1cea7c48
Matt Charlton <Matt.charlton@noredink.com>
2019-09-10 10:58:58 +0100

    query selectors

---
+++ chapter_6/dom.js
@@ -0,0 +1,11 @@
+const para = document.querySelector('p')
+console.log(para)
+
+const divs = document.querySelectorAll('div')
+console.log(divs)
+console.log(divs[0])
+
+
+divs.forEach(div => {
+  console.log(div)
+})
