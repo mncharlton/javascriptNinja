0c5e24cb2393bda6c62a651ad1899da513ba10b2
Matt Charlton <Matt.charlton@noredink.com>
2019-09-10 11:21:47 +0100

    attributes

--- chapter_6/dom.js
+++ chapter_6/dom.js
@@ -5,7 +5,28 @@ const divs = document.querySelectorAll('div')
 console.log(divs)
 console.log(divs[0])
 
-
 divs.forEach(div => {
   console.log(div)
 })
+
+
+para.innerText += ' Woop Woop!'
+
+divs.forEach(div => {
+  //div.innerText = 'Changed! No worries.'
+  div.innerHTML += '<p><b>BOLD ME</b></p>'
+})
+
+
+const staff = ['mario', 'luigi', 'yoshi']
+
+staff.forEach(name => {
+  para.innerHTML += `<p>${name}</p>`
+})
+
+
+const link = document.querySelector('a')
+console.log(link.getAttribute('href'))
+
+link.setAttribute('href', 'www.yahoo.co.uk') //edit attribute
+link.setAttribute('style', 'color:green') //add attribute
