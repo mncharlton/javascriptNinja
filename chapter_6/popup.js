536fcacfb24e8beadfa911e26ece2896edee0bff
Matt Charlton <Matt.charlton@noredink.com>
2019-09-13 11:58:12 +0100

    making a popup

---
+++ chapter_6/popup.js
@@ -0,0 +1,17 @@
+const button = document.querySelector('button')
+const popup = document.querySelector('.popup-wrapper')
+const popupClose = document.querySelector('.popup-close')
+
+button.addEventListener('click', () => {
+  popup.style.display = 'block'
+})
+
+popupClose.addEventListener('click', () => {
+  popup.style.display = 'none'
+})
+
+popup.addEventListener('click', e => {
+  if(e.target.className === 'popup-wrapper') {
+    popup.style.display = 'none'
+  }
+})
