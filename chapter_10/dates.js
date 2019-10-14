75e9a57bf3f913a784a9dcf25eda7235542b75ae
Matt Charlton <Matt.charlton@noredink.com>
2019-10-14 10:49:51 +0100

    dates and timestamps

---
+++ chapter_10/dates.js
@@ -0,0 +1,28 @@
+const now = new Date()
+
+console.log('Year:', now.getFullYear())
+console.log('Month:', now.getMonth()) //0 indexed
+console.log('Day:', now.getDay()) //0 indexed
+console.log('Date:', now.getDate())
+console.log('Hrs', now.getHours())
+console.log('Mins:', now.getMinutes())
+console.log('Seconds:', now.getSeconds())
+console.log('Time:', now.getTime())
+
+
+console.log(now.toDateString())
+console.log(now.toTimeString())
+console.log(now.toLocaleString())
+
+
+
+//timestamps
+const before = new Date('February 1 2019 7:30:59')
+const diff = now.getTime() - before.getTime()
+console.log(diff)
+const mins = Math.round(diff / 1000 / 60)
+console.log(mins)
+
+//convert timestamp into dates
+const timestamp = 1675938472356
+console.log(new Date(timestamp))
