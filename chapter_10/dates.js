268298b3632f94a8a39e00c4fe497a3b5fd7c470
Matt Charlton <Matt.charlton@noredink.com>
2019-10-14 11:03:13 +0100

    change chapter 3 folder name and make a digital clock

--- chapter_10/dates.js
+++ chapter_10/dates.js
@@ -26,3 +26,20 @@ console.log(mins)
 //convert timestamp into dates
 const timestamp = 1675938472356
 console.log(new Date(timestamp))
+
+
+//digital clock
+const clock = document.querySelector('.clock')
+
+const tick = () => {
+  const current = new Date()
+  let h = current.getHours()
+  if (h < 10) {h = `0${h}`}
+  let m = current.getMinutes()
+  if (m < 10) {m = `0${m}`}
+  let s = current.getSeconds()
+  if (s < 10) {s = `0${s}`}
+  clock.innerHTML = `<p>${h}:${m}:${s}</p>`
+}
+
+setInterval(tick, 1000)
