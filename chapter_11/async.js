9848897b4adf02168c1b13017ff19eac82c99e0e
Matt Charlton <Matt.charlton@noredink.com>
2019-10-21 18:55:07 +0100

    html request

--- chapter_11/async.js
+++ chapter_11/async.js
@@ -7,3 +7,23 @@ setTimeout(() => {
 
 console.log(3)
 console.log(4)
+
+
+//HTTP Request
+const request = new XMLHttpRequest(); //doesn't need to be XML
+
+
+//listen  for Request
+request.addEventListener('readystatechange', () => {
+  console.log(request, request.readyState)
+  //Check mozilla mdn guide for ready state change codes
+  if (request.readyState === 4) {
+    console.log(request.responseText)
+  }
+})
+
+//Set up request
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+
+//send Request
+request.send()
