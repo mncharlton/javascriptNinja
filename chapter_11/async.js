e7f2376c9d52600077cfc77e7a1d5fae36317e52
Matt Charlton <Matt.charlton@noredink.com>
2019-10-21 19:11:28 +0100

    as a function

--- chapter_11/async.js
+++ chapter_11/async.js
@@ -8,17 +8,17 @@ setTimeout(() => {
 console.log(3)
 console.log(4)
 
-
 //HTTP Request
 const request = new XMLHttpRequest(); //doesn't need to be XML
-
 
 //listen  for Request
 request.addEventListener('readystatechange', () => {
   console.log(request, request.readyState)
-  //Check mozilla mdn guide for ready state change codes
-  if (request.readyState === 4) {
+  //Check mozilla mdn guide for ready state change codes and return codes
+  if (request.readyState === 4 && request.status === 200) {
     console.log(request.responseText)
+  } else if (request.readyState === 4) {
+    console.log('An error occured!')
   }
 })
 
@@ -27,3 +27,26 @@ request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
 
 //send Request
 request.send()
+
+
+//function to get todos
+const getTodos = () => {
+  const todoRequest = new XMLHttpRequest(); //doesn't need to be XML
+  todoRequest.addEventListener('readystatechange', () => {
+    console.log(todoRequest, todoRequest.readyState)
+    //Check mozilla mdn guide for ready state change codes and return codes
+    if (todoRequest.readyState === 4 && todoRequest.status === 200) {
+      console.log(todoRequest.responseText)
+    } else if (todoRequest.readyState === 4) {
+      console.log('An error occured!')
+    }
+  })
+  todoRequest.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+  todoRequest.send()
+}
+
+getTodos()
+
+getTodos(() => {
+
+})
